8f08f00203c46a0df0410f7b500818cfa35bba48
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-25 10:17:30 +0200

    Delivers https://jvtthkjira.atlassian.net/browse/KAN-3

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080;
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/games', (req, res) => {
+    res.send(["Team Fortress 2", "Sonic the Edgehog", "STALKER", "STALKER 2"])
+})
+
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => {console.log(`Backend api jookseb aadressil: http://localhost:${port}`);});
