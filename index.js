07c59c21809a44bae8bcedbd0e5181c9456d2595
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-25 11:17:32 +0200

    Delivers https://jvtthkjira.atlassian.net/browse/KAN-14

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const app = require('express')();
 const port = 8080;
 const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
+const yamljs = require('yamljs');
+/* const swaggerDocument = require('./docs/swagger.json'); */
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+
+const games = 
+[
+    {id: 1, name: "Team Fortress 2", releaseEU : "2007", description: "Pootispencarhiar. class based lootbox shooter", reviewscore: 86},{id: 2, name: "Sonic the Hedgehog", releaseEU : "1991", description: "Speedy blue platformer", reviewscore: 77},
+    {id: 3, name: "S.T.A.L.K.E.R.: Shadow of Chernobyl ", releaseEU : "2007", description: "shoot in ukraine", reviewscore: 82},,
+    {id: 4, name: "S.T.A.L.K.E.R. 2: Heart of Chornobyl ", releaseEU : "2007", description: "shoot in ukraine again", reviewscore: 78},
+];
 
 app.get('/games', (req, res) => {
-    res.send(["Team Fortress 2", "Sonic the Edgehog", "STALKER", "STALKER 2"])
+    res.send(games);
+})
+
+app.get('/games/:id', (req, res) => {
+    if(typeof games[req.params.id] === 'undefined') {
+        return res.status(404).send({Error: 'Game not found'});
+    }
+    res.send(games[req.params.id-1]);
 })
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
