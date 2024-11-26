1fd87c0a6f4f3aa00dc81abbf4df0a6e51a9519b
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-26 14:08:32 +0200

    Delivers #3 https://jvtthkjira.atlassian.net/jira/software/projects/KAN/boards/1?selectedIssue=KAN-14

--- index.js
+++ index.js
@@ -7,9 +7,27 @@ const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
 const games = 
 [
-    {id: 1, name: "Team Fortress 2", releaseEU : "2007", description: "Pootispencarhiar. class based lootbox shooter", reviewscore: 86},{id: 2, name: "Sonic the Hedgehog", releaseEU : "1991", description: "Speedy blue platformer", reviewscore: 77},
-    {id: 3, name: "S.T.A.L.K.E.R.: Shadow of Chernobyl ", releaseEU : "2007", description: "shoot in ukraine", reviewscore: 82},,
-    {id: 4, name: "S.T.A.L.K.E.R. 2: Heart of Chornobyl ", releaseEU : "2007", description: "shoot in ukraine again", reviewscore: 78},
+    {
+        id: 1, 
+        name: "Team Fortress 2", releaseEU : "2007", description: "Pootispencarhiar. class based lootbox shooter", reviewscore: 86
+    },
+    {
+        id: 2, 
+        name: "Sonic the Hedgehog", releaseEU : "1991", description: "Speedy blue platformer", 
+        reviewscore: 77
+    },
+    {
+        id: 3, 
+        name: "S.T.A.L.K.E.R.: Shadow of Chernobyl ", 
+        releaseEU : "2007", description: "shoot in ukraine", 
+        reviewscore: 82
+    },        
+    {
+        id: 4, 
+        name: "S.T.A.L.K.E.R. 2: Heart of Chornobyl ", 
+        releaseEU : "2007", description: "shoot in ukraine again", 
+        reviewscore: 78
+    },
 ];
 
 app.get('/games', (req, res) => {
@@ -17,8 +35,11 @@ app.get('/games', (req, res) => {
 })
 
 app.get('/games/:id', (req, res) => {
-    if(typeof games[req.params.id] === 'undefined') {
+    if(typeof games[req.params.id-1] === 'undefined') {
         return res.status(404).send({Error: 'Game not found'});
+    }
+    if(typeof games[req.params.id] == null) {
+        return res.status(400).send({error: 'invalid game id'});
     }
     res.send(games[req.params.id-1]);
 })
