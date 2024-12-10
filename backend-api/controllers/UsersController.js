1ce2909f06d37e311098f3e4467840418c66a378
Estlib <75094683+Estlib@users.noreply.github.com>
2024-12-10 16:23:16 +0200

    Delivers #1 #2 https://jvtthkjira.atlassian.net/browse/KAN-40?atlOrigin=eyJpIjoiYWEzMGJkNTA3NmJjNDVhZjhmYjc3NWE4ZWMwNTRhOGQiLCJwIjoiaiJ9

--- backend-api/controllers/UsersController.js
+++ backend-api/controllers/UsersController.js
@@ -9,3 +9,24 @@ async (req, res) => {
         .map(({id, firstname, lastname}) => {return id, firstname, lastname}));
 }
 
+exports.getById =
+async (req, res) => {
+    const user = await getUser(req, res);
+    if (!user) { return res.status(404).send({Error: 'User not found'});}
+    res.send(user);
+}
+
+const getUser = 
+async (req, res) => {
+    const idNumber = parseInt(req.params.id);
+    if(isNaN(idNumber)) {
+        res.status(400).send({Error: `ID must be a whole number: ${idNumber}`});
+        return null;
+    }
+    const user = await db.users.findByPk(idNumber);
+    if(!user) {
+        res.status(404).send({Error: `user with this id not found: ${idNumber}`});
+        return null;
+    }
+    return user;
+}
