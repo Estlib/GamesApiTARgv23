babe4e89cbe56ad3c1c016bcd7f28b45139e8cba
Estlib <75094683+Estlib@users.noreply.github.com>
2025-01-07 14:26:08 +0200

    .js moodul .vue mooduliks

---
+++ frontend/src/components/game/GameDetails.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    
+    props: ["gameInModal"]
+}
+</script>
+
+<template>
+    <table class="table table-striped">
+        <tr>
+            <th>Id</th>
+            <th>{{gameInModal.id}}</th>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <th>{{gameInModal.name}}</th>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <th>{{gameInModal.releaseEU}}</th>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <th>{{gameInModal.description}}</th>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <th>{{gameInModal.reviewscore}}</th>
+        </tr>
+    </table>
+</template>
+
